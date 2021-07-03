696851fa3eea1e7e6cbef6e2b36538ce64b3d41d
MunirahAr <munirahalarwan@hotmail.com>
2021-07-03 21:01:18 +0300

    Add files via upload

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +11,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+
+# I add postgresql url below as you show 
+SQLALCHEMY_DATABASE_URI = '<postgresql://localhost/munsanadb?user=other&password=sss>'
+
+# otherwise
+
+SQLALCHEMY_DATABASE_URI = os.environ['mysql://munsana:123456@server/db']
+
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://postgresql://localhost/munsanadb?user=other&password=sss", 1)
+
+SQL_TRACK_MODIFICATIONS = False
+
